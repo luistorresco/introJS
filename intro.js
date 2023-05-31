766965eb78ce68b7019ebafcae956bd56bb33e3d
Miguel Angel Sanchez Valencia <sanchomini01@hotmail.com>
2023-05-31 11:29:19 -0500

    introduccion a js

---
+++ intro.js
@@ -0,0 +1,34 @@
+// alert ("hola desde forma externa")
+//tipos de ventanas
+
+//aler()-> sirve para mostrar un mensaje en el navegador 
+// alert("la suma de 4 + 4 = "+(4+4));
+// var num1 = 5;
+// var num2 = 7;
+// var nombre = "juan";
+// alert("la suma" +num1+ "+" +num2+ "=" +(num1+num2) )
+// alert("su nombre es"+nombre);
+// confirm("¿?")
+
+// var respuestas = confirm("¿es mayor de edad?");
+// alert("su respuesta es"+respuestas);
+
+// var respuesta = prompt("digite su nombre");
+// alert("su respuesta es "+respuesta);
+
+
+//var num1 = prompt("digite un numero");
+//var num2 = prompt("digite otro numero");
+//alert("su respuesta es "+(num1/num2));
+
+//parseInt()
+//parseFloat()
+var num3 =parseInt(prompt("digite otro numero"));
+var num4 =parseInt(prompt("digite un cuarto numero"));
+//parseInt -> convierte el texto en numero 
+//parseFloat() -> convierte el texto en numero decimal 
+alert("su numero es "+(num3+num4));
+var resultado = num3+num4;
+console.log("su resultado es "+(num3+num4));
+document.write("su numero es "+(num3+num4));
+document.write("<h1> el resultado es:"+ resultado+"</h1>");
